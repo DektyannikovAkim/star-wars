81cbda4886b995217b570a28c241b3d2a5007734
Akim <dektyannikov.akim@mail.ru>
2021-07-29 02:26:01 +0700

    Исправил поиск

--- searchComp.js
+++ searchComp.js
@@ -7,19 +7,33 @@ const search = {
     methods: {
         searchHeroes(userSearch) {
             this.$parent.heroes.length = 0;
-            this.$parent.getJson(this.$parent.API).then(data => {
-                data.results.forEach(item => {
-                    if (item.name.toLowerCase().includes(userSearch.toLowerCase())) {
-                        item.id = this.$parent.getIdFromUrl(item.url);
-                        item.characterPhoto = 'https://starwars-visualguide.com/assets/img/characters/' + item.id + '.jpg';
-                        item.favorite = this.$parent.favoritesHeroes.find(el => el.id === item.id);
-                        this.$parent.getJson(item.homeworld).then(data => {
-                            item.homeworld = data.name;
-                        });
-                        this.$parent.heroes.push(item);
-                    }
-                })
+            this.$parent.getJson('https://swapi.dev/api/people/?page=' + this.$parent.pagination.currentPage + '&format=json').then(data => {
+                this.enumOfValues(data, userSearch);
+                if (this.$parent.heroes.length === 0 && data.next) {
+                    this.searchNextPage(data.next, userSearch);
+                }
             })
+        },
+        searchNextPage(nextURL, userSearch) {
+            this.$parent.getJson(nextURL).then(data => {
+                this.enumOfValues(data, userSearch);
+                if (this.$parent.heroes.length === 0 && data.next) {
+                    this.searchNextPage(data.next, userSearch);
+                }
+            })
+        },
+        enumOfValues(data, userSearch) {
+            data.results.forEach(item => {
+                if (item.name.toLowerCase().includes(userSearch.toLowerCase())) {
+                    item.id = this.$parent.getIdFromUrl(item.url);
+                    item.characterPhoto = 'https://starwars-visualguide.com/assets/img/characters/' + item.id + '.jpg';
+                    item.favorite = this.$parent.favoritesHeroes.find(el => el.id === item.id);
+                    this.$parent.getJson(item.homeworld).then(data => {
+                        item.homeworld = data.name;
+                    });
+                    this.$parent.heroes.push(item);
+                }
+            });
         }
     },
     template: `
